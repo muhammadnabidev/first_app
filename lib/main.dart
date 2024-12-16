7da3e7a83f99358b89e276fb9bf94b123d6fcbee
unknown <muhmmadnabimamtov2012@gmail.com>
2024-12-16 19:08:09 +0500

    shop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:muhammad_nabi/screens/splash_screen.dart';
+import 'package:muhammad_nabi/pages/shop_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: ShopPage(),
     );
   }
 }
--- lib/pages/navigation/fovorites_page.dart
+++ lib/pages/navigation/fovorites_page.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_slider/carousel_slider.dart';
 
-class FovoritesPage extends StatelessWidget {
+class FovoritesPage extends StatefulWidget {
   const FovoritesPage({super.key});
 
   @override
+  State<FovoritesPage> createState() => _FovoritesPageState();
+}
+
+class _FovoritesPageState extends State<FovoritesPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Fovorite_page'),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      body: CarouselSlider(
+          slideTransform: CubeTransform(),
+          slideIndicator: CircularSlideIndicator(
+            padding: EdgeInsets.only(bottom: 50),
+            currentIndicatorColor: Colors.white,
+          ),
+          unlimitedMode: true,
+          children: [
+            Container(
+              color: const Color.fromARGB(200, 0, 35, 189),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 2, 116, 2),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 255, 251, 18),
+            ),
+            Container(
+              color: const Color.fromARGB(255, 255, 54, 54),
+            )
+          ]),
     );
   }
 }
